61b3cec76b7844af09d88c6352307797e2a96e44
Igor Egorov <igor.egroff@yandex.ru>
2022-01-23 15:49:53 +0300

    Первоначальная версия

---
+++ src/main/java/Client.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        String localhost = "127.0.0.1";
+        int port = 8089;
+
+        try (Socket clientSocket = new Socket(localhost, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("Igor");
+            String resp = in.readLine();
+            System.out.println(resp);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
